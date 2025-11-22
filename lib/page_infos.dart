bac505dda834cf028f0972e27fc9e990ff8b35c3
adel bennacer <adelbennacer46gmail.com>
2025-11-22 15:37:34 -0500

    verifie application page infos

--- lib/page_infos.dart
+++ lib/page_infos.dart
@@ -1,23 +1,27 @@
 import 'package:flutter/material.dart';
 
 class PageInfos extends StatelessWidget {
-PageInfos({super.key});
+ const PageInfos({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
+    return  Scaffold(
         appBar: AppBar(title: Text("page d'infos")),
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("bienvenue sur la page infos !"),
+              const Text("bienvenue sur la page infos !"),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("page precedente"),
+              )
             ],
           ),
         ),
-      ),
-    );
+      );
     
-  }
+     }
 }
